c988ab373550dda42860821d7dd1846bd0fe7ffb
Ananta Bhowmik <ananta@nsales.dk>
2024-09-25 16:27:59 +0600

    copy-db: code optimise

--- app/Http/Requests/Database/CopyDB.php
+++ app/Http/Requests/Database/CopyDB.php
@@ -25,20 +25,6 @@ class CopyDB extends FormRequest
     {
         return [
             'selectedDatabases' => 'required|array',
-
-//            'selectedCompanies' => 'required|exists:mysql.Company,Id',
-//            'name' => 'required|unique:mysql.Table,Name',
-//            'type' => 'required|in:Server,Client,Both',
-//            'database' => 'required|in:Company,Office,Both',
-//            'module' => 'required',
-//            'selectedCompanies' => 'sometimes|array',
-//            'disabled' => 'required|boolean',
-//            'clientSync' => 'nullable|in:Download,Both',
-//            'autoNumbering' => 'required|boolean',
-//            'enableTruncate' => 'required|boolean',
-//            'sqlTruncate' => 'nullable',
-//            'sqlSeed' => 'nullable',
-//            'note' => 'nullable'
         ];
     }
 }
--- resources/js/data/menu.js
+++ resources/js/data/menu.js
@@ -258,7 +258,7 @@ export default {
         {
             name: "Horizon",
             to: "/horizon",
-            icon: "far fa-eye",
+            icon: "fa fa-tachometer-alt",
             directLink: true,
             targetBlank: true
         },
