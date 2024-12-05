79e1b77561d4266ef8620b4b38fc25b369a0a037
Shovha Saha <shovhasaha@Shovhas-MacBook-Pro.local>
2024-12-05 13:08:43 +0600

    Button added to activate not installed modules

--- resources/js/components/onboard/ModuleEnable.vue
+++ resources/js/components/onboard/ModuleEnable.vue
@@ -1,5 +1,14 @@
 <template>
-    <h4 class="">Enable Modules</h4>
+    <h4 class="d-flex justify-content-between">
+        Enable Modules
+        <button class="btn btn-sm btn-outline-success"
+                v-if="notInstalledModules && notInstalledModules.length > 0"
+                :disabled="isLoading"
+                @click.prevent="activateNotInstalledModules"
+        >
+            Activate Modules
+        </button>
+    </h4>
     <div class="card mb-2" v-if="modules" v-for="(module, moduleName) in modules">
         <div class="card-body p-3 d-flex justify-content-between">
             <div>{{ moduleName }}</div>
@@ -7,19 +16,23 @@
                 <small class="text-success" v-if="module.Installed === true">
                     <i class="fa fa-check"></i> Active
                 </small>
-                <small class="text-muted" v-else>
+                <small class="text-muted" v-else-if="isLoading">
                     <i class="fa fa-circle-notch fa-spin rounded-circle p-1"></i>
                     Activating
+                </small>
+                <small class="text-danger" v-else>
+                    Not active
                 </small>
             </div>
         </div>
     </div>
 </template>
 <script setup>
-import {defineProps, onMounted, ref, watch} from "vue";
+import {computed, defineProps, onMounted, ref, watch} from "vue";
 import {useCompanyStore} from "@/stores/companyStore";
 import Module from "@/models/Office/Module";
 import _ from "lodash";
+import Button from "@/components/ui/Button.vue";
 
 const props = defineProps(['name', 'step']);
 const emits = defineEmits(['complete']);
@@ -27,6 +40,8 @@ const emits = defineEmits(['complete']);
 const companyStore = useCompanyStore();
 
 const modules = ref(null);
+
+const isLoading = ref(false)
 async function getCompanyModules() {
     let {data} = await Module.getActivatedAndAvailableModulesByCompany(companyStore.selectedCompany.Id);
 
@@ -42,12 +57,30 @@ async function getCompanyModules() {
             if(props.step.payload.modules.includes(module.Name)) {
                 temp[module.Name] = module
                 temp[module.Name]["Installed"] = false
-                setTimeout(() => activateModule(module), 10000)
             }
         }
 
         modules.value = temp
     }
+}
+
+const notInstalledModules = computed(() => {
+    if(modules.value) {
+        return Object.values(modules.value).filter(module => !module.Installed)
+    }
+    return []
+})
+
+const activateNotInstalledModules = () => {
+    isLoading.value = true
+    const promises = []
+    for (let module of notInstalledModules.value) {
+        promises.push(activateModule(module))
+    }
+
+    Promise.all(promises).then(() => {
+        emits("complete", true)
+    })
 }
 
 async function activateModule(module) {
@@ -64,8 +97,8 @@ watch(() => companyStore.getSelectedCompany, (newSelectedCompany) => {
     }
 });
 
-watch(modules, (modifiedVal) => {
-    if(!Object.values(modifiedVal).map(module => module.Installed).includes(false)) {
+watch(notInstalledModules, (modifiedVal) => {
+    if(!modifiedVal || !modifiedVal.length) {
         emits("complete", true)
     }
 })
