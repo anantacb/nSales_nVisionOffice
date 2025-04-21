f3be321f99c38bb4598bd20f4418e149ef42bffd
Swadhin Saha <swadhin@nsales.dk>
2025-04-21 18:40:13 +0600

    Cache clear added

---
+++ resources/js/views/global-helper/CacheClear.vue
@@ -0,0 +1,38 @@
+<script setup>
+import {ref} from "vue";
+import {useTemplateStore} from "@/stores/templateStore";
+import GlobalHelper from '@/models/GlobalHelper'
+
+let successMessage = ref("");
+const templateStore = useTemplateStore();
+
+async function cacheClear() {
+    templateStore.pageLoader({mode: "on"});
+    let {message} = await GlobalHelper.cacheClear();
+    successMessage.value = message;
+    templateStore.pageLoader({mode: "off"});
+}
+
+cacheClear();
+</script>
+
+<template>
+    <!-- Page Content -->
+    <div class="hero">
+        <div class="hero-inner text-center">
+            <div class="bg-body-extra-light">
+                <div class="content content-full overflow-hidden">
+                    <div class="py-4">
+                        <h1 class="h1 fw-normal">
+                            {{ successMessage }}
+                        </h1>
+                    </div>
+                </div>
+            </div>
+            <div class="content content-full text-muted fs-sm fw-medium">
+                <a class="link-fx" href="/">Go Home</a>
+            </div>
+        </div>
+    </div>
+    <!-- END Page Content -->
+</template>
