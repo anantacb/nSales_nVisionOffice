5cc86503b47132a3a52e8df8d18eedb03b8177d1
Ananta Bhowmik <anantabhowmik16@gmail.com>
2025-06-27 16:36:00 +0600

    db migration draft

--- app/Helpers/Helpers.php
+++ app/Helpers/Helpers.php
@@ -2,10 +2,72 @@
 
 namespace App\Helpers;
 
+use Exception;
+use Illuminate\Encryption\Encrypter;
 use Illuminate\Support\Carbon;
+use Illuminate\Support\Facades\App;
+use Illuminate\Support\Facades\Config;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
 
 class Helpers
 {
+    /**
+     * @throws Exception
+     */
+    public static function connectDB($dbName): void
+    {
+        try {
+            Config::set("database.connections.mysql_company.database", $dbName);
+            DB::purge('mysql_company');
+            DB::connection('mysql_company')->getPdo();
+            Log::info('Connected to DB: ' . $dbName);
+        } catch (Exception $exception) {
+            Log::error('Error connecting to DB: ' . $exception->getMessage());
+            throw new Exception('DB not found', 400);
+        }
+    }
+
+    /**
+     * @throws Exception
+     */
+    public static function connectCloudSqlDB($company): void
+    {
+        try {
+            $databaseHost = $company->DatabaseHost;
+            $databaseUser = $company->DatabaseUser;
+            $databasePassword = $company->DatabasePassword;
+            if (!empty($databasePassword)) {
+                $encrypter = new Encrypter(env('DB_ENCRYPTION_KEY', ''), config('app.cipher'));
+                $databasePassword = $encrypter->decrypt($databasePassword);
+            }
+            $databaseName = $company->DatabaseName;
+
+            if (!empty($databaseHost)) {
+                if (!App::environment('production')) {
+                    $databaseHost = env('DEV_GOOGLE_SQL_HOST', '10.30.0.13');
+                }
+                Config::set("database.connections.mysql_company.host", $databaseHost);
+            }
+            if (!empty($databaseUser)) {
+                Config::set("database.connections.mysql_company.username", $databaseUser);
+            }
+            if (!empty($databasePassword)) {
+                Config::set("database.connections.mysql_company.password", $databasePassword);
+            }
+            if (!empty($databaseName)) {
+                Config::set("database.connections.mysql_company.database", $databaseName);
+            }
+            // dd(Config::get("database.connections.mysql_company"));
+            DB::purge('mysql_company');
+            DB::connection('mysql_company')->getPdo();
+            Log::info('Connected to Cloud SQL DB: ' . $databaseName, $databaseHost);
+        } catch (Exception $exception) {
+            Log::error('Error connecting to Cloud SQL DB: ' . $exception->getMessage());
+            throw new Exception('DB not found', 400);
+        }
+    }
+
 
     /**
      * @param Carbon $date
